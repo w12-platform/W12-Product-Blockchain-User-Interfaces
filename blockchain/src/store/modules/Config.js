17fede779759cf0931d39f098bbc6834e798ce3d
Plevako Semen <Semapl3@yandex.ru>
2018-09-05 21:24:59 +0600

    feat: up config W12Lister

--- blockchain/src/store/modules/Config.js
+++ blockchain/src/store/modules/Config.js
@@ -4,7 +4,7 @@ export default {
     namespaced: true,
     state: {
         W12Lister: {
-            address: "0xc32fdbdd68847db9ffeb886105fbda80e6c5ceef"
+            address: "0x2729b859ed267cc33f21cc44ebb0e9f4be8ad45c"
         },
         FactoryTokens: {
             address: "0x2729b859ed267cc33f21cc44ebb0e9f4be8ad45c"
