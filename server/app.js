009fb77c5518999ae4764901e4433b7d97f3acf2
Hokid <mr.hokid@gmail.com>
2018-09-06 17:10:59 +0000

    fix: try fix Cloudflare error

--- server/app.js
+++ server/app.js
@@ -41,16 +41,16 @@ app.get('/project',function(req,res){
 
 const proxyUrl = process.env.PROXY_URL;
 
-if (proxyUrl) {
-  app.use('/', proxy(proxyUrl, {
-    preserveHostHdr: true,
-    proxyReqPathResolver: function (RQ) {
-      return RQ.originalUrl;
-    }
-  }));
-} else {
-  app.use('/', api);
-}
+// if (proxyUrl) {
+//   app.use('/', proxy(proxyUrl, {
+//     preserveHostHdr: true,
+//     proxyReqPathResolver: function (RQ) {
+//       return RQ.originalUrl;
+//     }
+//   }));
+// } else {
+//   app.use('/', api);
+// }
 
 app.use(fallbackHistory('index.html', { root: APP_DIR }));
 // catch 404 and forward to error handler
