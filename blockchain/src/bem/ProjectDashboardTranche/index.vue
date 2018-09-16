a547d8137860d5601826bb001eef9ace28c73097
Plevako Semen <Semapl3@yandex.ru>
2018-09-16 09:02:07 +0600

    feat: tranche event

--- blockchain/src/bem/ProjectDashboardTranche/index.vue
+++ blockchain/src/bem/ProjectDashboardTranche/index.vue
@@ -40,12 +40,15 @@
     import Milestones from 'bem/Milestones';
     import TrancheInformation from 'bem/TrancheInformation';
 
+    import {CONFIRM_TX} from "store/modules/Transactions.js";
     import {createNamespacedHelpers} from 'vuex';
+    import {isZeroAddress} from 'lib/utils';
 
     const LedgerNS = createNamespacedHelpers("Ledger");
     const AccountNS = createNamespacedHelpers("Account");
     const ProjectNS = createNamespacedHelpers("Project");
     const LangNS = createNamespacedHelpers("Lang");
+    const ConfigNS = createNamespacedHelpers('Config');
 
     export default {
         name: 'ProjectDashboardTranche',
@@ -72,10 +75,14 @@
                 currentAccountData: "currentAccountData",
             }),
             ...ProjectNS.mapState({
+                currentProject: "currentProject",
                 ProjectMeta: "meta",
             }),
             ...LangNS.mapState({
                 langMeta: 'meta'
+            }),
+            ...ConfigNS.mapState({
+                W12Lister: "W12Lister"
             }),
 
             isError() {
@@ -93,6 +100,10 @@
                 handler: 'handleCurrentAccountChange',
                 immediate: true
             },
+            'ProjectMeta': {
+                handler: 'handleProjectMetaChange',
+                deep: true,
+            }
         },
         methods: {
             ...AccountNS.mapActions({
@@ -100,14 +111,70 @@
                 updateAccountData: 'updateAccountData',
             }),
             ...ProjectNS.mapActions({
-                ProjectFetchList: "fetchList"
+                ProjectFetchList: "fetchList",
+                updateFundInformation: "updateFundInformation",
+            }),
+            ...LedgerNS.mapActions({
+                LedgerFetch: 'fetch',
             }),
 
             async handleCurrentAccountChange(currentAccount) {
                 if(currentAccount){
                     await this.ProjectFetchList();
                 }
-            }
+            },
+
+            async handleProjectMetaChange(meta) {
+                if(!meta.loadingProject) {
+                    await this.updateAccountData();
+                    this.unsubscribeFromEvents();
+                    await this.subscribeToEvents();
+                    window.dispatchEvent(new Event('resize'));
+                }
+            },
+            unsubscribeFromEvents() {
+                if (!this.subscribedEvents) return;
+
+                if(this.subscribedEvents.StagesUpdated){
+                    this.subscribedEvents.TrancheReleased.stopWatching();
+                }
+
+                this.subscribedEvents = null;
+            },
+            async subscribeToEvents() {
+                if (!this.currentProject) return;
+                if (this.subscribedEvents) return;
+
+                this.subscribeToEventsLoading = true;
+
+                try {
+                    const {W12CrowdsaleFactory, W12FundFactory} = await this.LedgerFetch();
+                    let TrancheReleased = null;
+
+                    if (!isZeroAddress(this.currentProject.crowdsaleAddress)) {
+                        const W12Crowdsale = W12CrowdsaleFactory.at(this.currentProject.crowdsaleAddress);
+                        const fundAddress = await W12Crowdsale.methods.fund();
+                        const W12Fund = W12FundFactory.at(fundAddress);
+                        TrancheReleased = W12Fund.events.TrancheReleased(null, null, this.onTrancheReleasedEvent);
+                    }
+
+                    this.subscribedEvents = {
+                        TrancheReleased,
+                    };
+                } catch (e) {
+                    this.error = e.message;
+                }
+
+                this.subscribeToEventsLoading = false;
+            },
+            async onTrancheReleasedEvent(error, result) {
+                if (!error) {
+                    await this.updateFundInformation({Token: this.currentProject});
+                    await this.updateAccountData();
+                    const tx = result.transactionHash;
+                    this.$store.commit(`Transactions/${CONFIRM_TX}`, tx);
+                }
+            },
         },
         async created() {
             await this.watchCurrentAccount();
