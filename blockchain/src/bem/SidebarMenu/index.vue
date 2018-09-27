5122dded63ebecce5ca474276c934502a9537b5f
DenisProgrammer <denis@w12.io>
2018-09-27 13:03:34 +0300

    WD-18 Добавил в меню Yellowpaper

--- blockchain/src/bem/SidebarMenu/index.vue
+++ blockchain/src/bem/SidebarMenu/index.vue
@@ -3,6 +3,7 @@
         <a class="btn sidebar-btn warn" href="https://tokensale.w12.io/?utm_source=test-net&amp;utm_medium=demo_page" data-warn="!" target="_blank">{{ $t('SidebarMenuHowItWorks') }}</a>
         <a class="btn sidebar-btn" target="_blank" href="https://github.com/w12-platform/W12-Product-Blockchain-Protocol">{{ $t('SidebarMenuViewCodeProduct') }}</a>
         <a class="btn sidebar-btn" target="_blank" href="https://github.com/w12-platform/W12-Product-Blockchain-User-Interfaces">{{ $t('SidebarMenuViewCodeUI') }}</a>
+        <a class="btn sidebar-btn" target="_blank" :href="$t('SidebarMenuReadYellowpaperHref')">{{ $t('SidebarMenuReadYellowpaper') }}</a>
         <a class="btn sidebar-btn" target="_blank" href="https://tokensale.w12.io/?utm_source=test-net&amp;utm_medium=demo_page">{{ $t('SidebarMenuGoMarketplace') }}</a>
         <a class="btn sidebar-btn" target="_blank" :href="$t('SidebarMenuReadWhitePaperHref')">{{ $t('SidebarMenuReadWhitePaper') }}</a>
         <a class="btn sidebar-btn" target="_blank" :href="$t('SidebarMenuTelegramHref')">{{ $t('SidebarMenuTelegram') }}</a>
