e618253a7b7426551d75c65e9edecbc2f5866ce8
Plevako Semen <Semapl3@yandex.ru>
2018-09-29 13:27:17 +0300

    fix: bug performance in lang plugin

--- blockchain/src/components/App.js
+++ blockchain/src/components/App.js
@@ -5,7 +5,10 @@ import VueMask from 'v-mask';
 import Cleave from 'vue-cleave-component';
 import Tooltip from 'vue-directive-tooltip';
 import 'vue-directive-tooltip/css/index.css';
-import LangPlugin from 'plugin/LangPlugin';
+import {createNamespacedHelpers} from "vuex";
+import { LANG_UPDATE_META } from "store/modules/Lang.js";
+const LangNS = createNamespacedHelpers("Lang");
+
 
 import 'bem/buefy/default.scss';
 import store from "store";
@@ -18,8 +21,6 @@ Vue.use(Tooltip);
 Vue.use(vuexI18n.plugin, store, {
     translateFilterName: 't'
 });
-
-Vue.use(LangPlugin);
 
 import AdminDashboard from 'bem/AdminDashboard';
 import LangSwitch from 'bem/LangSwitch';
@@ -54,5 +55,52 @@ new Vue({
         ProjectDashboard,
         ProjectDashboardReceiving,
         ProjectDashboardTranche,
+    },
+    computed: {
+        ...LangNS.mapState({
+            currentLang: 'current',
+            vocabulary: 'vocabulary'
+        }),
+    },
+    methods: {
+        ...LangNS.mapActions({
+            getLang: "getLang",
+            getVocabulary: "getVocabulary"
+        }),
+    },
+    async created() {
+        if(this.$store){
+            await this.getLang();
+            await this.getVocabulary();
+            if(this.$i18n && this.vocabulary && this.currentLang){
+                this.$i18n.set(this.currentLang);
+
+                let labelsLang = [];
+                let arrayTranslations = {};
+                let translations = this.vocabulary;
+                for (const label in translations){
+                    for (const language in translations[label]) {
+                        if(labelsLang.indexOf(language) === -1 ){
+                            labelsLang.push(language);
+                        }
+                        if (translations[label].hasOwnProperty(language)) {
+                            if (!arrayTranslations.hasOwnProperty(language)) {
+                                arrayTranslations[language] = {};
+                            }
+                            arrayTranslations[language][label] = translations[label][language];
+                        }
+                    }
+                }
+
+                for (const language in arrayTranslations) {
+                    if (arrayTranslations.hasOwnProperty(language)) {
+                        this.$i18n.add(language, arrayTranslations[language]);
+                    }
+                }
+
+                this.$store.commit(`Lang/${LANG_UPDATE_META}`, {loading: false});
+                window.dispatchEvent(new Event('resize'));
+            }
+        }
     }
 });
