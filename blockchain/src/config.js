1828b892a496c92a1c1c55461d3128ac54c894ee
Kirill Khoroshilov <mr.hokid@gmail.com>
2018-10-17 20:32:22 +0700

    fix: sometimes lost data when requesting contract states through metamask
    Solution: request state through another provider
    
    Closes W12-121

--- blockchain/src/config.js
+++ blockchain/src/config.js
@@ -1,26 +1,17 @@
 const params = new URLSearchParams(window.location.search);
 
-const net = params.has('net') ? params.get('net') : 'https://ropsten.infura.io/';
-const key = params.has('key') ? params.get('key') : '7orv4QUSQbFWWcCpqHmH';
+const provider4 = 'https://rinkeby.infura.io/v3/8df9df41a48b4dd290a1abbba80b9953';
+const provider1 = 'https://mainnet.infura.io/v3/8df9df41a48b4dd290a1abbba80b9953';
 const feePercent = params.has('feePercent') ? params.get('feePercent') : '10';
 const apiTranslate = "https://w12.io/ru/api/translate/w12translations.json"
-const blockchainNetworkId = process.env.BLOCKCHAIN_NETWORK_ID || '4';
+const blockchainNetworkId = process.env.BLOCKCHAIN_NETWORK_ID || '1';
 //const W12Lister = "0x090fd7807410455b59b95c492fda165c4b5b5679";//params.has('W12Lister') ? params.get('W12Lister') : "";//store.state.Config.W12Lister.address;
 //const W12CrowdsaleFactory = params.has('W12CrowdsaleFactory') ? params.get('W12CrowdsaleFactory') : null;
 
 export default {
-    // place here your app config
-    infura: {
-        /*
-            Mainnet	production network	https://mainnet.infura.io/
-            Ropsten	test network	https://ropsten.infura.io/
-            INFURAnet	test network	https://infuranet.infura.io/
-            Kovan	test network	https://kovan.infura.io/
-            Rinkeby	test network	https://rinkeby.infura.io/
-            IPFS	gateway	https://ipfs.infura.io
-         */
-        net,
-        key
+    providers: {
+        4: provider4,
+        1: provider1
     },
     // if true then infura will be used as fallback
     useInfuraAsFallbackNet: true,
--- blockchain/src/store/modules/Config.js
+++ blockchain/src/store/modules/Config.js
@@ -6,22 +6,18 @@ export default {
     namespaced: true,
     state: {
         FactoryTokens: {
-            address: "0x15bfbebcace798a18c42c2ff1f23388449355ea4"
+            address: "0x146f81931a3e0a53f1847f33175f25b488f9fef8"
         },
         W12Lister: {
-            address: "0x4dda30a4ab8e05223a772e1bbf09e3bfb6c9aff3",
+            address: "0x4a3d202120d2d75445c73f256507f1b4186e25d4",
             version: "0.21.3"
         },
         Default: {
-            version: "0.20.5"
+            version: "0.21.3"
         },
         W12ListerList: [
             {
-                address: "0x0d60b4c74be0670166dc8f5588d35c8d50f4929d",
-                version: "0.20.5"
-            },
-            {
-                address: "0x4dda30a4ab8e05223a772e1bbf09e3bfb6c9aff3",
+                address: "0x4a3d202120d2d75445c73f256507f1b4186e25d4",
                 version: "0.21.3"
             },
         ],
