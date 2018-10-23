f98d4c15aff1d88ad96e125a6fa8b68d5f591eb1
Kirill Khoroshilov <mr.hokid@gmail.com>
2018-10-23 19:34:01 +0700

    feat: copy fixes onto last version

--- src/bem/ProjectStages/0.23.2/index.vue
+++ src/bem/ProjectStages/0.23.2/index.vue
@@ -42,11 +42,13 @@
         watch: {
             'ProjectMeta.loadingProject': {
                 handler: 'handleProjectChange',
+                immediate: true
             },
         },
         data() {
             return {
                 subscribeToEventsLoading: false,
+                isSubscribedToEvent: false,
                 error: false,
             };
         },
@@ -86,12 +88,15 @@
                 updateProject: "updateProject"
             }),
 
-            async handleProjectChange() {
-                await this.updateAccountData();
-                this.unsubscribeFromEvents();
-                await this.subscribeToEvents();
+            async handleProjectChange (loadingStatus) {
+                if (loadingStatus === false) {
+                    await this.updateAccountData();
+                    this.unsubscribeFromEvents();
+                    await this.subscribeToEvents();
+                }
             },
             unsubscribeFromEvents() {
+                if (!this.isSubscribedToEvent) return;
                 if (!this.subscribedEvents) return;
 
                 this.subscribedEvents.CrowdsaleInitialized.stopWatching();
@@ -116,12 +121,13 @@
                 }
 
                 this.subscribedEvents.TokenPlaced.stopWatching();
-
+                this.isSubscribedToEvent = false;
                 this.subscribedEvents = null;
             },
             async subscribeToEvents() {
                 if (!this.currentProject) return;
                 if (this.subscribedEvents) return;
+                if (this.isSubscribedToEvent) return;
 
                 this.subscribeToEventsLoading = true;
 
@@ -136,8 +142,8 @@
                     let UnsoldTokenReturned = null;
                     let TrancheReleased = null;
 
-                    if (!isZeroAddress(this.currentProject.crowdsaleAddress)) {
-                        const W12Crowdsale = W12CrowdsaleFactory.at(this.currentProject.crowdsaleAddress);
+                    if (!isZeroAddress(this.currentProject.tokenCrowdsaleAddress)) {
+                        const W12Crowdsale = W12CrowdsaleFactory.at(this.currentProject.tokenCrowdsaleAddress);
                         const fundAddress = await W12Crowdsale.methods.fund();
                         const W12Fund = W12FundFactory.at(fundAddress);
                         StagesUpdated = W12Crowdsale.events.StagesUpdated(null, null, this.onStagesUpdatedEvent);
@@ -174,6 +180,7 @@
                 }
 
                 this.subscribeToEventsLoading = false;
+                this.isSubscribedToEvent = true;
             },
 
             async onTrancheReleasedEvent(error, result) {
