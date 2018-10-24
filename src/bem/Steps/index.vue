ae76931eae5fa64ec3aa8c91a173056db138343c
Семен Плевако <semapl3@yandex.ru>
2018-10-25 00:05:57 +0300

    feat: remove Next Step btn

--- src/bem/Steps/index.vue
+++ src/bem/Steps/index.vue
@@ -3,12 +3,6 @@
         <section class="container">
             <div class="row align-items-center">
                 <div class="Steps__text column text-left">{{ $t('StepsText', { number }) }}</div>
-                <div class="Steps__btn column text-right" v-if="number !== all">
-                    <div class="labelTooltip" v-if="blocked" v-tooltip="blocked">
-                        <button class="Steps__btn btn btn-primary py-2 my-2" @click="next" disabled>{{ $t('StepsNext') }}</button>
-                    </div>
-                    <button v-else class="Steps__btn btn btn-primary py-2 my-2" @click="next">{{ $t('StepsNext') }}</button>
-                </div>
             </div>
         </section>
     </div>
@@ -24,18 +18,8 @@
             number: {
                 required: true
             },
-            blocked: {
-            },
-            link: {
-                type: String,
-            },
             all: {
                 default: 10
-            }
-        },
-        methods: {
-            next(){
-                document.location.href = this.link.toString();
             }
         },
     };
