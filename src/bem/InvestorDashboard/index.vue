72ce0af77654380346095154065580a476126518
Семен Плевако <semapl3@yandex.ru>
2018-11-13 00:11:45 +0300

    feat: add RoadMap table

--- src/bem/InvestorDashboard/index.vue
+++ src/bem/InvestorDashboard/index.vue
@@ -19,6 +19,7 @@
                 <TokenSwitch v-if="!isCurrentToken"></TokenSwitch>
                 <component :is="CalculatorComponent"></component>
                 <component :is="SaleTableComponent"></component>
+                <RoadMap></RoadMap>
                 <component :is="CrowdSaleComponent"></component>
             </div>
         </section>
@@ -34,6 +35,7 @@
 
     import TokenSwitch from 'bem/TokenSwitch';
     import Steps from "bem/Steps";
+    import RoadMap from "bem/RoadMap";
 
     const LedgerNS = createNamespacedHelpers("Ledger");
     const AccountNS = createNamespacedHelpers("Account");
@@ -60,7 +62,8 @@
         },
         components: {
             TokenSwitch,
-            Steps
+            Steps,
+            RoadMap
         },
         data() {
             return {
