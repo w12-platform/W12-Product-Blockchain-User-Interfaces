c55f7f1f54c02298ce5cc0eb7fab51762228a5a5
Семен Плевако <semapl3@yandex.ru>
2018-11-13 14:06:11 +0300

    feat: loader

--- src/bem/InvestorDashboardExchange/index.vue
+++ src/bem/InvestorDashboardExchange/index.vue
@@ -10,8 +10,7 @@
             </b-notification>
 
             <b-notification v-if="isLoading && !isError" :closable="false" class="InvestorDashboardExchange__loader">
-                <span v-if="ledgerMeta.loading">{{ $t('InvestorDashboardLoadLedger') }}<br></span>
-                <span v-if="tokensListMeta.loading">{{ $t('InvestorDashboardLoadTokens') }}<br></span>
+                <span v-html="$t('InvestorDashboardLoadLedger')"></span><br>
 
                 <b-loading :is-full-page="false" :active="isLoading" :can-cancel="true"></b-loading>
             </b-notification>
