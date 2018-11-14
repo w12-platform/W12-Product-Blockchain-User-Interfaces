3a483991cf2b3add1c7a5e72b61ee8d888d0fed5
Hokid <mr.hokid@gmail.com>
2018-11-14 15:17:18 +0700

    ci: update scripts

--- scripts/generate-bundle.sh
+++ scripts/generate-bundle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-tar -cf bundle.tar build/ stand/ *.html translations.js package.json package-lock.json serve.js
+tar -cf bundle.tar build/ stand/ *.html translations.js package.json package-lock.json serve.js node_modules/moment/ node_modules/bootstrap/
