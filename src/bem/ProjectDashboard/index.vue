dae1531b509f8c2c52900e1fda5b1beb9e85ca90
Semapl3 <Semapl3@yandex.ru>
2018-11-19 15:53:32 +0300

    fix: loadingError bug (#72)

--- src/bem/ProjectDashboard/index.vue
+++ src/bem/ProjectDashboard/index.vue
@@ -69,7 +69,7 @@
                 langMeta: 'meta'
             }),
             isError() {
-                return this.ledgerMeta.loadingError || this.ProjectMeta.loadingProjectError || this.accountMeta.loadingError;
+                return this.ledgerMeta.loadingError || this.ProjectMeta.loadingError || this.accountMeta.loadingError;
             },
             isLoading() {
                 return (
